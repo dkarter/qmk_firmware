8a5d9ebe574521048d43bdc4fa4d6ed7314181a5
Dorian Karter <dkarter@gmail.com>
2016-02-07 21:32:37 -0600

    Add macros and shuffle keys

--- keyboard/ergodox_ez/keymaps/doriank_osx/keymap.c
+++ keyboard/ergodox_ez/keymaps/doriank_osx/keymap.c
@@ -6,22 +6,28 @@
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
 
+enum macros {
+  M_RSHFT = 0,
+  M_QUICK_COLN = 1,
+  M_STICKY_SHFT = 2
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  |  [   |           |   ]  |   6  |   7  |   8  |   9  |   0  |   -    |
+ * | GRV    |   1  |   2  |   3  |   4  |   5  |  [   |           |   ]  |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | TAB    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  :   |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | TAB    |   Q  |   W  |   E  |   R  |   T  |  =   |           |  :   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |ESC/Ctrl|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|   '    |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | ESC    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  | ;/:  |   '    |
+ * |--------+------+------+------+------+------|CTRL+Z|           |CTRL+W|------+------+------+------+------+--------|
+ * |SFT/CAPS|Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  `\~ | CTRL |  Alt | Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   |  L1  | CTRL |  Alt | Left | Right|                                       | Left | Down |  Up  | Right|  L2  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |CTRL+Z|CTRL+W|       | PgUp | ESC  |
+ *                                        | Hyper| Meh  |       | PgUp | ESC  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | END  |       | PgDn |        |      |
  *                                 | Space|Backsp|------|       |------|  CTRL  |Enter |
@@ -32,20 +38,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,          KC_3,   KC_4,   KC_5,   KC_LBRC,
-        KC_TAB,         KC_Q,         KC_W,          KC_E,   KC_R,   KC_T,   TG(SYMB),
-        CTL_T(KC_ESC),  KC_A,         KC_S,          KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,  KC_X,  KC_C,          KC_V,   KC_B,   ALL_T(KC_NO),
-        KC_GRV,         KC_LCTRL,     KC_LALT,       KC_LEFT,KC_RGHT,
-                                              LCTL(KC_Z),  LCTL(KC_W),
+        KC_GRV,               KC_1,         KC_2,          KC_3,     KC_4,   KC_5,   KC_LBRC,
+        KC_TAB,               KC_Q,         KC_W,          KC_E,     KC_R,   KC_T,   KC_EQL,
+        KC_ESC,               KC_A,         KC_S,          KC_D,     KC_F,   KC_G,
+        M(M_STICKY_SHFT),     CTL_T(KC_Z),  KC_X,          KC_C,     KC_V,   KC_B,   LCTL(KC_Z),
+        TG(SYMB),             KC_LCTRL,     KC_LALT,       KC_LEFT,  KC_RGHT,
+                                              ALL_T(KC_NO),  MEH_T(KC_NO),
                                                               KC_END,
                                                KC_SPC,KC_BSPC,KC_LGUI,
         // right hand
-        KC_RBRC,          KC_6,   KC_7,    KC_8,     KC_9,     KC_0,              KC_MINS,
-        LSFT(KC_SCLN),    KC_Y,   KC_U,    KC_I,     KC_O,     KC_P,              KC_BSLS,
-                          KC_H,   KC_J,    KC_K,     KC_L,     LT(MDIA, KC_SCLN), KC_QUOT,
-        MEH_T(KC_NO),     KC_N,   KC_M,    KC_COMM,  KC_DOT,   CTL_T(KC_SLSH),    KC_RSFT,
-                          KC_UP,  KC_DOWN, KC_LBRC,  KC_RBRC,  KC_FN1,
+        KC_RBRC,          KC_6,    KC_7,    KC_8,     KC_9,     KC_0,            KC_MINS,
+        KC_COLN,          KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,            KC_BSLS,
+                          KC_H,    KC_J,    KC_K,     KC_L,     M(M_QUICK_COLN), KC_QUOT,
+        LCTL(KC_W),       KC_N,    KC_M,    KC_COMM,  KC_DOT,   CTL_T(KC_SLSH),  KC_RSFT,
+                          KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT,  TG(MDIA),
         KC_PGUP, KC_ESC,
         KC_PGDN,
         KC_RALT, KC_RCTRL, KC_ENT
@@ -97,13 +103,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      | MsUp |      |      |      |           |      |MsWhUp|      |      |      |      |        |
+ * |        |      |      | MsUp |      |      |      |           |      |MsWhDn|      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|MsWhDn|      |      |      |      |  Play  |
+ * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|MsWhUp|      |      |      |      |  Play  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      | Prev | Next |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | Lclk | Rclk |                                       |VolUp |VolDn | Mute |      |      |
+ *   |      |      |      | Lclk | Rclk |                                       |      |VolUp |VolDn | Mute |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -125,10 +131,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_BTN1, KC_BTN2, KC_TRNS,
     // right hand
        KC_TRNS,  KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-       KC_TRNS,  KC_MS_WH_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                 KC_MS_WH_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
+       KC_TRNS,  KC_MS_WH_DOWN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                 KC_MS_WH_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
        KC_TRNS,  KC_TRNS,       KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-                                KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
+                                KC_TRNS, KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_WFWD, KC_WBAK
@@ -139,19 +145,71 @@ const uint16_t PROGMEM fn_actions[] = {
     [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
 };
 
+static uint16_t semicolon_key_timer;
+static uint16_t semicolon_taps = 0;
+static uint16_t lshift_key_timer;
+static uint16_t lshift_taps = 0;
+
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 {
   // MACRODOWN only works in this function
-      switch(id) {
-        case 0:
-        if (record->event.pressed) {
-          register_code(KC_RSFT);
-        } else {
-          unregister_code(KC_RSFT);
-        }
-        break;
+  switch(id) {
+    case M_RSHFT:
+      if (record->event.pressed) {
+        register_code(KC_RSFT);
+      } else {
+        unregister_code(KC_RSFT);
       }
-    return MACRO_NONE;
+      break;
+    case M_QUICK_COLN:
+      if (record->event.pressed) {
+        if (!semicolon_key_timer || timer_elapsed(semicolon_key_timer) > 200) {
+          semicolon_taps = 1;
+        } else {
+          semicolon_taps += 1;
+        }
+
+        semicolon_key_timer = timer_read();
+
+        if (semicolon_taps == 1) {
+          register_code(KC_SCLN);
+        } else {
+          // delete semicolon
+          register_code(KC_BSPC);
+          unregister_code(KC_BSPC);
+
+          // insert colon
+          register_code(KC_LSFT);
+          register_code(KC_SCLN);
+          unregister_code(KC_SCLN); unregister_code(KC_LSFT);
+        }
+      } else {
+        unregister_code(KC_SCLN);
+      }
+      break;
+    case M_STICKY_SHFT:
+      if (record->event.pressed) {
+        if (!lshift_key_timer || timer_elapsed(lshift_key_timer) > 200) {
+          lshift_taps = 1;
+        } else {
+          lshift_taps += 1;
+        }
+
+        lshift_key_timer = timer_read();
+
+        if (lshift_taps == 1) {
+          register_code(KC_LSFT);
+        } else {
+          register_code(KC_CAPS);
+          unregister_code(KC_CAPS);
+        }
+      } else {
+        unregister_code(KC_LSFT);
+      }
+      break;
+  }
+
+  return MACRO_NONE;
 };
 
 // Runs just one time when the keyboard initializes.
